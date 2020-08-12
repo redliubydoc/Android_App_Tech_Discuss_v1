7c53c7553332861b979de9330a9c30245d637bc8
increible <rajeswar.sarkar.2009@gmail.com>
2020-08-12 14:25:02 +0530

    "All basic fuctions of WebView working properly, splash not added "

--- app/src/main/java/com/epizy/techdiscuss/MainActivity.java
+++ app/src/main/java/com/epizy/techdiscuss/MainActivity.java
@@ -3,12 +3,52 @@ package com.epizy.techdiscuss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* tracking web view wiget */
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        /* creating object of web viwe settings */
+
+        WebSettings webSettings =  webView.getSettings();
+
+        /* enabling javascript in web view */
+
+        webSettings.setJavaScriptEnabled(true);
+
+        /* loafing tech discuss url */
+
+        webView.loadUrl("http://www.techdiscuss.epizy.com");
+
+        /* to make the app itself as a web browser */
+
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    /* to make android back button act as web browser back button */
+
+    @Override
+    public void onBackPressed()
+    {
+        if(webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 }
