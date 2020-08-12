42cdccbdcf2d55d4869994d4734d80a1beaf9cb5
increible <rajeswar.sarkar.2009@gmail.com>
2020-08-12 21:47:40 +0530

    "CLEARTEXT_NOT_PERMITTED error fixed"

---
+++ app/src/main/java/com/epizy/techdiscuss/Constants.java
@@ -0,0 +1,11 @@
+package com.epizy.techdiscuss;
+
+class Constants
+{
+    public static final String TECH_DISCUSS_URL;
+
+    static
+    {
+        TECH_DISCUSS_URL = "http://www.techdiscuss.epizy.com";
+    }
+}
